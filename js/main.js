391921174e0b63ead667da7efb103a4b23862cd6
airej-2022 <114749347+airej-2022@users.noreply.github.com>
2022-10-07 14:15:08 +0100

    first comit

---
+++ js/main.js
@@ -0,0 +1,82 @@
+
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+includeHTML();
+$(window).on("load", function () {
+  //$('.preloader-wrapper').fadeOut();
+  $("body").removeClass("preloader-site");
+});
+// Open the full screen search box
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+// Close the full screen search box
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
+
+// Aboja3fer
+
+setTimeout(() => {
+  $(".preloader-wrapper").fadeOut();
+  // $('body').removeClass('preloader-site');
+}, 2000);
+// $('.preloader-wrapper').fadeOut();
+
+inlineSVG.init(
+  {
+    svgSelector: "img.svg-inline", // the class attached to all images that should be inlined
+    initClass: "js-inlinesvg", // class added to <html>
+  },
+  function () {
+    console.log("All SVGs inlined");
+  }
+);
+AOS.init();
+
+var acc = document.getElementsByClassName("accordion-btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    this.parentElement.classList.toggle("open");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
